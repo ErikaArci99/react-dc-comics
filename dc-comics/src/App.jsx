d46caeca123af7500fe961c1aff06f501a2d95a3
ErikaArci99 <erikaarci1999@outlook.it>
2025-05-05 16:18:15 +0200

    [UPD] creati e collegati main header e footer

--- dc-comics/src/App.jsx
+++ dc-comics/src/App.jsx
@@ -1,13 +1,17 @@
-import './App.css'
+import './index.css'
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <Header />
+      <Main />
+      <Footer />
     </>
   )
 }
 
-export default App
+export default App;
