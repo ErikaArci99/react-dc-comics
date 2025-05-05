407019f031d63af56c5e2522b1df9dfff9e4a538
ErikaArci99 <erikaarci1999@outlook.it>
2025-05-05 17:13:12 +0200

    [UPD] fatta prima parte header

--- dc-comics/src/components/Header.jsx
+++ dc-comics/src/components/Header.jsx
@@ -2,7 +2,27 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <header>Header</header>
+        <header>
+            <div className="container d-flex justify-content-between">
+                <img src="../public/img/dc-logo.png" alt="logo" className="my-3" />
+                <div className="fw-bold mt-5">
+                    <a href="" className='me-3 text-dark-emphasis'>CHARACTERS</a>
+                    <a href="" className='me-3 text-primary bottom-bar'>COMICS</a>
+                    <a href="" className='me-3 text-dark-emphasis'>MOVIES</a>
+                    <a href="" className='me-3 text-dark-emphasis'>TV</a>
+                    <a href="" className='me-3 text-dark-emphasis'>GAMES</a>
+                    <a href="" className='me-3 text-dark-emphasis'>COLLECTIBLES</a>
+                    <a href="" className='me-3 text-dark-emphasis'>VIDEOS</a>
+                    <a href="" className='me-3 text-dark-emphasis'>FANS</a>
+                    <a href="" className='me-3 text-dark-emphasis'>NEWS</a>
+                    <a href="" className='me-3 text-dark-emphasis'>SHOP</a>
+                </div>
+            </div>
+            <div className=''></div>
+            <div className='bg-dark'>
+                hello
+            </div>
+        </header>
     );
 };
 
