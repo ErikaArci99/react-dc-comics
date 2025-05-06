b0a65106f375763facf4c28e3d7da652995e2e29
ErikaArci99 <erikaarci1999@outlook.it>
2025-05-06 16:34:57 +0200

    [UPD] creata componente card statica

---
+++ dc-comics/src/components/ComicCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ComicCard = () => {
+    return (
+        <div className="comic-card">
+            <h2>Titolo Statico</h2>
+            <p>Informazioni di prova</p>
+        </div>
+    );
+};
+
+export default ComicCard;
