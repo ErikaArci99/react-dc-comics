7c55f4b301fe1c03e58865450e4b93c4e6cc6d4c
ErikaArci99 <erikaarci1999@outlook.it>
2025-05-06 10:09:52 +0200

    [FIX] svuotata cartella app.css e aggiunta responsività alla pagina con un hamburger menu

--- dc-comics/src/components/Header.jsx
+++ dc-comics/src/components/Header.jsx
@@ -3,20 +3,43 @@ import React from 'react';
 const Header = () => {
     return (
         <header>
-            {/* intestazione */}
-            <div className="container d-flex justify-content-between">
-                <img src="../public/img/dc-logo.png" alt="logo" className="my-3" />
-                <div className="fw-bold mt-5">
-                    <a href="" className='me-3 text-dark-emphasis'>CHARACTERS</a>
-                    <a href="" className='me-3 text-primary bottom-bar'>COMICS</a>
-                    <a href="" className='me-3 text-dark-emphasis'>MOVIES</a>
-                    <a href="" className='me-3 text-dark-emphasis'>TV</a>
-                    <a href="" className='me-3 text-dark-emphasis'>GAMES</a>
-                    <a href="" className='me-3 text-dark-emphasis'>COLLECTIBLES</a>
-                    <a href="" className='me-3 text-dark-emphasis'>VIDEOS</a>
-                    <a href="" className='me-3 text-dark-emphasis'>FANS</a>
-                    <a href="" className='me-3 text-dark-emphasis'>NEWS</a>
-                    <a href="" className='me-3 text-dark-emphasis'>SHOP</a>
+            <div className="container">
+                <div className="row align-items-center">
+
+                    {/* Logo */}
+                    <div className="col-6">
+                        <img src="../public/img/dc-logo.png" alt="logo" className="my-3" />
+                    </div>
+
+                    {/* Hamburger button - visibile su sm e md */}
+                    <div className="col-6 d-flex justify-content-end d-lg-none">
+                        <button
+                            className="btn btn-outline-secondary mt-3"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#mobileMenu"
+                            aria-controls="mobileMenu"
+                            aria-expanded="false"
+                            aria-label="Toggle navigation"
+                        >
+                            &#9776;
+                        </button>
+                    </div>
+
+                    {/* Menu links - collapse per sm e md, visibili da lg in poi */}
+                    <div className="fw-bold mt-5 col-12 col-lg-6 collapse d-lg-flex justify-content-lg-end text-uppercase" id="mobileMenu">
+                        <a href="" className='me-3 text-dark-emphasis'>CHARACTERS</a>
+                        <a href="" className='me-3 text-primary bottom-bar'>COMICS</a>
+                        <a href="" className='me-3 text-dark-emphasis'>MOVIES</a>
+                        <a href="" className='me-3 text-dark-emphasis'>TV</a>
+                        <a href="" className='me-3 text-dark-emphasis'>GAMES</a>
+                        <a href="" className='me-3 text-dark-emphasis'>COLLECTIBLES</a>
+                        <a href="" className='me-3 text-dark-emphasis'>VIDEOS</a>
+                        <a href="" className='me-3 text-dark-emphasis'>FANS</a>
+                        <a href="" className='me-3 text-dark-emphasis'>NEWS</a>
+                        <a href="" className='me-3 text-dark-emphasis'>SHOP</a>
+                    </div>
+
                 </div>
             </div>
         </header>
