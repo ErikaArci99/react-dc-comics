093021e5f5041a97f5aa32bf1cbbdbde0dc2b03e
ErikaArci99 <erikaarci1999@outlook.it>
2025-05-06 18:31:31 +0200

    [UPD] finita stilizzazione card

--- dc-comics/src/components/ComicCard.jsx
+++ dc-comics/src/components/ComicCard.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const ComicCard = ({ comic }) => {
     return (
-        <div className='col-6 col-md-4 col-lg-2'>
-            <img src={comic.thumb} alt={comic.title} className='card-img' />
-            <h3>{comic.series}</h3>
+        <div className='col-6 col-md-4 col-lg-2 mb-4'>
+            <div className="card-img">
+                <img src={comic.thumb} alt={comic.title} className='img' />
+            </div>
+            <h6 className='text-light'>{comic.series}</h6>
         </div>
     );
 };
