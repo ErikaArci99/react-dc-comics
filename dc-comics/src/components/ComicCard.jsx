023b2a4c5c3298071922993bf223c5d0c1ce495a
ErikaArci99 <erikaarci1999@outlook.it>
2025-05-07 10:14:52 +0200

    [FIX] corretti gli errori nelle props

--- dc-comics/src/components/ComicCard.jsx
+++ dc-comics/src/components/ComicCard.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const ComicCard = ({ comic }) => {
+const ComicCard = (props) => {
     return (
         <div className='col-6 col-md-4 col-lg-2 mb-4'>
             <div className="card-img">
-                <img src={comic.thumb} alt={comic.title} className='img' />
+                <img src={props.comic.thumb} alt={props.comic.title} className='img' />
             </div>
-            <h6 className='text-light'>{comic.series}</h6>
+            <h6 className='text-light'>{props.comic.series}</h6>
         </div>
     );
 };
